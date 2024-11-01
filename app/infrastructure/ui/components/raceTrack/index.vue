29350ab7e650a8583546144226091f60a305136c
Ankit Karnany <ankitkarnany@gmail.com>
2024-11-01 16:43:58 +0100

    chore: track rendering for brazil bug fix

--- app/infrastructure/ui/components/raceTrack/index.vue
+++ app/infrastructure/ui/components/raceTrack/index.vue
@@ -13,42 +13,60 @@ const map = ref();
 const track = ref();
 const { circuit } = toRefs(circuitStoreAdapter());
 
-// Watch
+// Watch for circuit data
 watch(
   () => circuit.value,
   async () => {
     mapboxgl.accessToken =
       'pk.eyJ1Ijoibmt0a2FybmFueSIsImEiOiJjbTJ5bHl5MnAwMjF6Mm1zOGpidXFlYTFoIn0.Xa2nwsPqC98sFQ9fTils4Q';
 
-    map.value = new mapboxgl.Map({
-      container: track.value,
-      style: 'mapbox://styles/mapbox/light-v11', // Mapbox style
-      center: [-99.092, 19.402], // Map center coordinates
-      zoom: 15
-    });
+    if (circuit.value?.geoJSON && circuit.value.geoJSON.bbox) {
+      const [minLng, minLat, maxLng, maxLat] = circuit.value.geoJSON.bbox;
 
-    map.value.on('load', () => {
-      // Add GeoJSON source to the map
-      map.value.addSource('circuit-track', {
-        type: 'geojson',
-        data: circuit.value?.geoJSON
+      map.value = new mapboxgl.Map({
+        container: track.value,
+        style: 'mapbox://styles/mapbox/light-v11',
+        center: [(minLng + maxLng) / 2, (minLat + maxLat) / 2], // Initial approximate center
+        zoom: 10 // Initial zoom level
       });
 
-      // Add a line layer to display the circuit track
-      map.value.addLayer({
-        id: 'circuit-track-line',
-        type: 'line',
-        source: 'circuit-track',
-        layout: {
-          'line-join': 'round',
-          'line-cap': 'round'
-        },
-        paint: {
-          'line-color': '#FF1E00', // Track line color
-          'line-width': 3 // Track line width
+      map.value.on('load', () => {
+        if (circuit.value) {
+          // Add GeoJSON source to the map
+          map.value.addSource('circuit-track', {
+            type: 'geojson',
+            data: circuit.value.geoJSON
+          });
+
+          // Add a line layer to display the circuit track
+          map.value.addLayer({
+            id: 'circuit-track-line',
+            type: 'line',
+            source: 'circuit-track',
+            layout: {
+              'line-join': 'round',
+              'line-cap': 'round'
+            },
+            paint: {
+              'line-color': '#FF1E00', // Track line color
+              'line-width': 3 // Track line width
+            }
+          });
+
+          // Fit map to the bounds of the GeoJSON track
+          map.value.fitBounds(
+            [
+              [minLng, minLat],
+              [maxLng, maxLat]
+            ],
+            {
+              padding: 20, // Optional padding around the bounds
+              maxZoom: 15 // Optional maximum zoom level
+            }
+          );
         }
       });
-    });
+    }
   }
 );
 
